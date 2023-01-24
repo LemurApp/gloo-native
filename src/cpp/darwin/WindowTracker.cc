b8487c5cfc1744031578fec207ac50d523a25ca9
Aaron Villalpando <aaron@gloo.chat>
2023-01-24 15:16:48 -0800

    only do cfrelease

--- src/cpp/darwin/WindowTracker.cc
+++ src/cpp/darwin/WindowTracker.cc
@@ -68,14 +68,15 @@ void GetWindowData(CFDictionaryRef window, WindowData &parsed) {
     parsed.position.height = rect.size.height;
   }
 }
-void GetAllWindows(std::vector<WindowData> & windows) {
+
+std::vector<WindowData> GetAllWindows() {
   // Fetch all windows which are on screen.
   CFArrayRef windowList = CGWindowListCopyWindowInfo(
       kCGWindowListOptionOnScreenOnly, kCGNullWindowID);
 
   // Find the window with the matching windowId
   const CFIndex numWindows = CFArrayGetCount(windowList);
-  windows.resize(numWindows);
+  std::vector<WindowData> windows(numWindows);
   for (CFIndex i = 0; i < numWindows; i++) {
     CFTypeRef window_unsafe_ = CFArrayGetValueAtIndex(windowList, i);
     if (CFGetTypeID(window_unsafe_) != CFDictionaryGetTypeID()) {
@@ -85,12 +86,12 @@ void GetAllWindows(std::vector<WindowData> & windows) {
     GetWindowData(window, windows[i]);
   }
   CFRelease(windowList);
+  return windows;
 }
 
 void WindowTracker::UpdateWindows() {
   while (runWorker_.load()) {
-    std::vector<WindowData> data;
-    GetAllWindows(data);
+    const auto data = GetAllWindows();
     bool hasStatusIndicator = false;
     for (const WindowData &w : data) {
       if (w.parentName.has_value() &&
